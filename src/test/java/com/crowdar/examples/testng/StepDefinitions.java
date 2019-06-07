b0b5bbde22baac9529f3e0cd780ad581ea7536d7
dario.vallejos <dario.vallejos@crowdaronline.com>
2019-06-06 21:00:36 -0300

    Adding last DriverManager configuration support

--- src/test/java/com/crowdar/examples/testng/StepDefinitions.java
+++ src/test/java/com/crowdar/examples/testng/StepDefinitions.java
@@ -5,7 +5,7 @@ import com.crowdar.core.PageSteps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
+import org.testng.Assert;
 
 
 public class StepDefinitions extends PageSteps {
