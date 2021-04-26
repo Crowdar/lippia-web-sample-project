e908e05b4b35121e1dfe6ff3bc55251a7df71c39
mspinelli <mauro.spinelli@crowdaronline.com>
2021-04-26 15:55:00 -0300

    fix

--- src/main/java/com/crowdar/examples/pages/GoogleHomePage.java
+++ src/main/java/com/crowdar/examples/pages/GoogleHomePage.java
@@ -18,7 +18,7 @@ public class GoogleHomePage extends PageBaseGoogle {
     }
 
     public void enterSearchCriteria(String text) {
-    	clickElement(By.xpath("//*[@id=\"introAgreeButton\"]/span/span"));
+    	clickElement(By.xpath("//*[contains(text(),'I agree')]"));
         completeField(By.xpath(INPUT_SEARCH_XPATH), text);
     }
 
