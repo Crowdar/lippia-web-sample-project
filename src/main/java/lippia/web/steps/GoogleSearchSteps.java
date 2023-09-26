79a469c93a907006f52558879442e240b01a54be
German Jofre <germanjofre.27@gma>
2023-09-26 20:17:53 -0300

    Finalizado

--- src/main/java/lippia/web/steps/GoogleSearchSteps.java
+++ src/main/java/lippia/web/steps/GoogleSearchSteps.java
@@ -7,24 +7,19 @@ import lippia.web.services.GoogleSearchResultService;
 
 public class GoogleSearchSteps extends PageSteps {
 
-    @Given("^The client is on google page$")
+    @Given("^El cliente se encuentra en el buscador$")
     public void home() {
         GoogleHomeService.navegarWeb();
     }
 
-    @Given("^The client isn't on google page$")
-    public void isNotHome() {
-        GoogleHomeService.navegarWeb();
-        throw new RuntimeException("the client is not on google page");
-    }
 
-    @When("^The client search for word (.*)$")
+    @When("^El cliente hace la (.*)$")
     public void search(String criteria) {
         GoogleHomeService.enterSearchCriteria(criteria);
         GoogleHomeService.clickSearchButton();
     }
 
-    @Then("The client verify that results are shown properly")
+    @Then("El cliente puede ver su busqueda")
     public void statVerification() {
         GoogleSearchResultService.verifyResults();
     }
