c201cc658f4f5f063d0b66873c6293e474efa4d1
Nestor <nestorcordoba@gmail.com>
2023-10-08 23:46:51 -0300

    08-10-2023 23:46

--- src/main/java/lippia/web/constants/HomeMenuConstants.java
+++ src/main/java/lippia/web/constants/HomeMenuConstants.java
@@ -4,7 +4,7 @@ package lippia.web.constants;
 
 public class HomeMenuConstants {
 
-    public static final String HOM_MENU_BUTTON = "xpath://*[@id='content']/nav/a";
+    public static final String HOM_MENU_BUTTON = "xpath://a[normalize-space()='Home']";
 
 
 }
--- src/main/java/lippia/web/services/HomeBanners.java
+++ src/main/java/lippia/web/services/HomeBanners.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 
 public class HomeBanners {
 
-    private static final String HOME_THREE_BANNER = "#n2-ss-6 > div.n2-ss-slider-1.n2-grab > div > div > div.n2-ss-slide.n2-ss-canvas.n2-ss-slide-active.n2-ss-slide-36 > div.n2-ss-slide-background > img";
+    private static final String HOME_THREE_BANNER = "img[alt='Shop Selenium Books']";
 
     public static void verifyPage() {
         Assert.assertTrue( ActionManager.waitPresence( HOME_THREE_BANNER ).isDisplayed() );
