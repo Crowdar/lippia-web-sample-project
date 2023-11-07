b833f1db76489b31bd3cf56627fa1c19fafe8b17
gonzalorgomez95 <110257653+gonzalorgomez95@users.noreply.github.com>
2023-11-07 18:21:40 -0300

    Se preparo el espacio de trabajo para las pruebas automatizadas

--- src/main/java/lippia/web/services/BookstoreHomeService.java
+++ src/main/java/lippia/web/services/BookstoreHomeService.java
@@ -2,20 +2,142 @@ package lippia.web.services;
 
 import com.crowdar.core.PropertyManager;
 import com.crowdar.core.actions.ActionManager;
+import com.crowdar.core.actions.WebActionManager;
+import com.crowdar.driver.DriverManager;
 import junit.framework.Assert;
 import lippia.web.constants.BookstoreConstants;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static com.crowdar.core.actions.WebActionManager.navigateTo;
+import static com.crowdar.driver.DriverManager.getDriverInstance;
 
-public class BookstoreHomeService extends ActionManager {
+public class BookstoreHomeService extends WebActionManager {
 
-    public static void navegarWeb(){
+    public static void webNavigation(){
         navigateTo(PropertyManager.getProperty("web.base.url"));
     }
 
-    /*public static void clickSearchButton() {
-        click(BookstoreConstants.SEARCH_BUTTON_NAME);
+    public static void homePageValidation(){
+        waitVisibility(BookstoreConstants.NEW_ARRIVALS_TITLE);
+        Assert.assertEquals( "new arrivals" , getText(BookstoreConstants.NEW_ARRIVALS_TITLE) );
     }
-    */
+
+    public static void navbarButtonClick(String boton) {
+        waitVisibility(BookstoreConstants.NAVBAR_LOGO);
+        if (!boton.equalsIgnoreCase("bookstore logo")) {
+            click(String.format(BookstoreConstants.NAVBAR_BUTTON), boton);
+        }else{
+            click(BookstoreConstants.NAVBAR_LOGO);
+            }
+        cerrarPublicidad();
+    }
+
+    public static void carouselImgValidation(int cantidad) {
+        waitVisibility(BookstoreConstants.CAROUSEL_IMG);
+        List<WebElement> cantImg = getElements(BookstoreConstants.CAROUSEL_IMG);
+        List<WebElement> flecha = getElements(BookstoreConstants.CAROUSEL_NEXT);
+        int contador = 0;
+
+        for (int i = 0; i < cantImg.size(); i++){
+            flecha.get(0).click();
+            contador++;
+        }
+
+        Assert.assertEquals(contador, cantidad);
+
+
+    }
+
+    public static void arrivalsValidation(int cantidad){
+        waitVisibility(BookstoreConstants.ARRIVAL_PRODUCTS);
+        List<WebElement> list = getElements(BookstoreConstants.ARRIVAL_PRODUCTS);
+        Assert.assertEquals(list.size(), cantidad);
+    }
+    public static void cerrarPublicidad() {
+
+        try{
+            WebElement publi = getElement(BookstoreConstants.CLOSE_AD);
+            if (publi.isDisplayed()) {
+                publi.click();
+                DriverManager.getDriverInstance().switchTo().defaultContent();
+            }
+        }catch(Exception efe) {
+            System.out.println(efe.getMessage());
+        }
+    }
+
+    public static void scrolluntil() {
+        waitVisibility(BookstoreConstants.NEW_ARRIVALS_TITLE);
+        scroll(getElement(BookstoreConstants.NEW_ARRIVALS_TITLE));
+    }
+
+    public static void scroll(WebElement element) {
+        JavascriptExecutor jse = DriverManager.getDriverInstance();
+        jse.executeScript("arguments[0].scrollIntoView()", new Object[]{element});
+    }
+
+    public static void clickeableImgValidation() {
+        waitVisibility(BookstoreConstants.ARRIVAL_PRODUCTS);
+        List<WebElement> arrivals = getElements(BookstoreConstants.ARRIVAL_PRODUCTS);
+        arrivals.get(0).click();
+    }
+
+    public static void urlValidation(String url) {
+        String urlActual = getDriverInstance().getCurrentUrl();
+        String urlCompleta = "";
+
+        if (urlActual.contains("product")) {
+            String urlBase = "https://practice.automationtesting.in/product/";
+            urlCompleta = urlBase.concat(url).concat("/");
+        }else{
+            String urlBase = "https://practice.automationtesting.in/";
+            urlCompleta = urlBase.concat(url).concat("/");
+        }
+
+        Assert.assertEquals(urlCompleta , urlActual);
+    }
+
+
+    public static void addButtonValidation() {
+        waitVisibility(BookstoreConstants.ADD_BUTTON);
+        Assert.assertEquals(true , isVisible(BookstoreConstants.ADD_BUTTON));
+    }
+
+    public static boolean isVisible(String locatorElement, String... locatorReplacementValue) {
+        return getElement(locatorElement, locatorReplacementValue).isDisplayed();
+    }
+
+
+    public static void enterUser(String email) {
+        waitVisibility(BookstoreConstants.USER_INPUT);
+        setInput(BookstoreConstants.USER_INPUT, email);
+        click(BookstoreConstants.LOGIN_FORM);
+    }
+
+    public static void enterPass(String pass) {
+        waitVisibility(BookstoreConstants.PASS_INPUT);
+        setInput(BookstoreConstants.PASS_INPUT, pass);
+    }
+
+    public static void loginButtonClick() {
+        waitVisibility(BookstoreConstants.LOGIN_BUTTON);
+        waitClickable(BookstoreConstants.LOGIN_BUTTON);
+        click(BookstoreConstants.LOGIN_BUTTON,false);
+    }
+
+    public static void errorValidation(String errormsg) {
+        waitVisibility(BookstoreConstants.LOGIN_ERROR_MSG);
+        String errorObtenido = getText(BookstoreConstants.LOGIN_ERROR_MSG);
+        Assert.assertEquals(errormsg , errorObtenido);
+    }
+
+    public static void loginFormValidation() {
+        waitVisibility(BookstoreConstants.LOGIN_FORM);
+        Assert.assertEquals(true , isVisible(BookstoreConstants.LOGIN_FORM));
+        Assert.assertEquals(true , isVisible(BookstoreConstants.LOGIN_BUTTON));
+    }
 }
 
--- src/main/java/lippia/web/steps/BookstoreSteps.java
+++ src/main/java/lippia/web/steps/BookstoreSteps.java
@@ -1,9 +1,12 @@
 package lippia.web.steps;
 
 import com.crowdar.core.PageSteps;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lippia.web.constants.BookstoreConstants;
+import lippia.web.listener.CerrarPublicidad;
 import lippia.web.services.BookstoreHomeService;
 import lippia.web.services.BookstoreMyAccountService;
 
@@ -11,16 +14,68 @@ public class BookstoreSteps extends PageSteps {
 
     @Given("^The client is on bookstore page$")
     public void home() {
-        BookstoreHomeService.navegarWeb();
+        BookstoreHomeService.webNavigation();
+        BookstoreHomeService.homePageValidation();
     }
 
-    /*@When("^The client looks for the title (.*)$")
-    public void search(String criteria) {
-        BookstoreHomeService.buscarPalabra("new arrivals");
-    }*/
+    @When("^The client clicks on (.*) on the menu bar$")
+    public void theClientClicksOnShopOnTheMenuBar(String boton) {
+        BookstoreHomeService.navbarButtonClick(boton);
+    }
 
-    /*@Then("The client verify that books are shown properly")
-    public void statVerification() {
-        BookstoreMyAccountService.verifyResults();
-    }*/
+    @Then("^Verifies the Home Page has (.*) arrivals")
+    public void verifiesTheHomePageHasArrivals(int cantidad) {
+    BookstoreHomeService.arrivalsValidation(cantidad);
+    }
+
+    @And("The client scrolls down on the home page")
+    public void theClientScrollsDownOnTheHomePage() {
+    BookstoreHomeService.scrolluntil();
+    }
+
+    @Then("^Verifies the Home Page has (.*) sliders")
+    public void verifiesTheHomePageHasThreeSliders(int cantidad) {
+        BookstoreHomeService.carouselImgValidation(cantidad);
+    }
+
+    @And("The client clicks on the first arrival image")
+    public void theClientClicksOnTheFirstArrivalImage() {
+        BookstoreHomeService.clickeableImgValidation();
+    }
+
+    @Then("Verifies that Add To Basket Button is visible to the client")
+    public void verifiesThatAddToBasketButtonIsVisibleToTheClient() {
+        BookstoreHomeService.addButtonValidation();
+    }
+
+    @And("^The client enters (.*) in username textbox")
+    public void theClientEntersTestuserAutomationYopmailComInUsernameTextbox(String email) {
+        BookstoreHomeService.enterUser(email);
+    }
+
+    @And("^The client enters (.*) in Password textbox")
+    public void theClientEntersTestinmgInPasswordTextbox(String pass) {
+        BookstoreHomeService.enterPass(pass);
+    }
+
+    @And("The client clicks on login button")
+    public void theClientClicksOnLoginButton() {
+        BookstoreHomeService.loginButtonClick();
+    }
+
+    @And("^Verifies that the client navigates to (.*) page")
+    public void verifiesThatTheClientNavigatesToPage(String url) {
+        BookstoreHomeService.urlValidation(url);
+    }
+
+    @Then("^Displays a message error with the text (.*)")
+    public void displaysAMessageErrorWithTheText(String errormsg) {
+        BookstoreHomeService.errorValidation(errormsg);
+    }
+
+    @And("Displays the login form for retrying login")
+    public void displaysTheLoginFormForRetryingLogin() {
+        BookstoreHomeService.loginFormValidation();
+    }
 }
+
